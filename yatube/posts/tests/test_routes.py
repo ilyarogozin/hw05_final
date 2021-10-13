3474920c4b5cef8e98479c28d9a77c8cf8cd9ef2
Ilya Rogozin <rogozin.il2399@gmail.com>
2021-10-13 19:19:04 +0400

    one more commit

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -9,19 +9,19 @@ POST_ID = 1
 class RoutesTest(TestCase):
     def test_addresses_matches_reverse_names(self):
         cases = [
-            ['posts:index', [], '/'],
-            ['posts:post_create', [], '/create/'],
-            ['posts:post_edit', [POST_ID], f'/posts/{ POST_ID }/edit/'],
-            ['posts:group_list', [SLUG], f'/group/{ SLUG }/'],
-            ['posts:profile', [USERNAME], f'/profile/{ USERNAME }/'],
-            ['posts:post_detail', [POST_ID], f'/posts/{ POST_ID }/'],
-            ['posts:follow_index', [], '/follow/'],
-            ['posts:profile_follow', [USERNAME],
+            ['index', [], '/'],
+            ['post_create', [], '/create/'],
+            ['post_edit', [POST_ID], f'/posts/{ POST_ID }/edit/'],
+            ['group_list', [SLUG], f'/group/{ SLUG }/'],
+            ['profile', [USERNAME], f'/profile/{ USERNAME }/'],
+            ['post_detail', [POST_ID], f'/posts/{ POST_ID }/'],
+            ['follow_index', [], '/follow/'],
+            ['profile_follow', [USERNAME],
              f'/profile/{ USERNAME }/follow/'],
-            ['posts:profile_unfollow', [USERNAME],
+            ['profile_unfollow', [USERNAME],
              f'/profile/{ USERNAME }/unfollow/'],
-            ['posts:add_comment', [POST_ID], f'/posts/{ POST_ID }/comment/'],
+            ['add_comment', [POST_ID], f'/posts/{ POST_ID }/comment/'],
         ]
-        for route, args, address in cases:
+        for route, args, url in cases:
             with self.subTest(route=route):
-                self.assertEqual(reverse(route, args=args), address)
+                self.assertEqual(reverse(f'posts:{ route }', args=args), url)
